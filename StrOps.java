2ac7682b9508d3b64dbd3aafdc55c5d99e20e17f
Michael Kim <chungdam0402@gmail.com>
2019-12-20 00:45:13 +0900

    Create StrOps.java
    ㅁㄴ

---
+++ StrOps.java
@@ -0,0 +1,15 @@
+class StrOps {
+  public static void main(String args[]) {
+    String str1 = "When it comes to Web Programming, Java is #1";
+    String str2 = new String(str1);
+    String str3 = "Java Strings are powerful.";
+    int result, idx;
+    char ch;
+
+    System.out.println("Length of str1: " + str1.length());
+
+    for(int i=0; i<str1.length(); i++) {
+
+    }
+  }
+}
